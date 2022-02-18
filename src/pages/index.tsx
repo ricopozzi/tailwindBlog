042eb414abee52a9183e07a90a5d05e96a1c89e0
unknown <ricopozzi@hotmail.com>
2022-02-17 21:55:17 -0300

    prismic CMS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import { Header } from '../components/Header'
 import { faker } from '@faker-js/faker'
 import { Avatar } from '../components/Avatar'
+import { GetStaticProps } from 'next'
+import { client } from '../../prismic-configuration'
 
-export default function Home() {
+export default function Home({posts}:any) {
 
   const generateParagraph = faker.lorem.lines(5)
   const anotherParagraph = faker.lorem.lines(5)
+
+  const paragraphClass = "w-10/12 mt-9 text-gray-800 font-normal text-xl text-justify leading-9 md:w-4/6"
 
   return (
     <>
@@ -13,32 +17,24 @@ export default function Home() {
     <section className="max-w-screen min-h-screen" >
         <img
         className="w-full h-96 object-cover"
-        src="https://blog.rocketseat.com.br/content/images/2019/03/Iniciando_com_GraphQL_no_NodeJS_e_ExpressJS.png" alt="graphql" 
+        src={posts.data.image_url.url} alt="graphql" 
         />
       <div
       className="w-11/12 md:w-4/8 h-full mx-auto flex items-center flex-col"
       >
         <h1
         className=" max-w-lg text-center font-bold font-sans text-4xl leading-[2.4rem] mt-14 "
-        >Por que o Facebook (Meta) criou o GraphQL?</h1>
+        >{posts.data.title[0].text}</h1>
         <p
         className="mt-4 mb-6 font-semibold"
         >4 de Fev de 2022</p>
-        <p
-        className="w-10/12 mt-9 text-gray-800 font-normal text-xl text-justify leading-9 md:w-4/6"
-        >{generateParagraph}</p>
 
-        <p
-        className="w-10/12 mt-9 text-gray-800 font-normal text-xl text-justify leading-9 md:w-4/6"
-        >{anotherParagraph}</p>
 
-        <p
-        className="w-10/12 mt-9 text-gray-800 font-normal text-xl text-justify leading-9 md:w-4/6"
-        >{anotherParagraph}</p>
+        {posts.data.main_text.map(par => (
+          <p key={par.text} className={paragraphClass} >{par.text}</p>
+        ) )}
 
-        <p
-        className="w-10/12 mt-9 text-gray-800 font-normal text-xl text-justify leading-9 md:w-4/6"
-        >{anotherParagraph}</p>
+
         <div
         className="w-full flex flex-col justify-center items-center"
         >
@@ -50,7 +46,20 @@ export default function Home() {
         
       </div>
     </section>
+    <footer
+    className="w-full h-6"
+    ></footer>
     </>
   )
 }
 
+
+export async function getStaticProps<GetStaticProps>() {
+  const posts = await client.getSingle("blog_post", {})
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
