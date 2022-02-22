05af31afedc716f01228f3e8fe8bfc6943528041
unknown <ricopozzi@hotmail.com>
2022-02-22 19:46:50 -0300

    carousel fix

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -31,7 +31,7 @@ export function Carousel({ posts }: CarouselProps) {
       className='w-full md:w-7/12 h-[25rem] rounded-lg bg-red-100  ] cursor-pointer
      shadow-md hover:shadow-2xl ease-in-out duration-500 hover:-translate-y-2 flex flex-row'
     >
-      <div className='w-full h-full ' onClick={handleCarouselClick}>
+      <div className='w-full h-full '>
         <div className=' w-full h-full relative select-none'>
           <img
             className=' w-full object-cover h-full rounded-lg'
@@ -39,7 +39,10 @@ export function Carousel({ posts }: CarouselProps) {
             alt=''
           />
 
-          <div className='absolute top-0 w-full rounded-lg h-full bg-slate-900 bg-opacity-40 md:p-12 flex items-center'>
+          <div
+            className='absolute p-6 top-0 w-full rounded-lg h-full bg-slate-900 bg-opacity-40 md:p-12 flex items-center'
+            onClick={handleCarouselClick}
+          >
             <h1 className='text-4xl text-slate-100 font-bold antialiased'>
               {posts[index].data.title[0].text}
             </h1>
